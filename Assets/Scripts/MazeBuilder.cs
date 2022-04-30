cb36a3d59e0f34b45ac687328ad4a80547ba8097
Asylbek Karashev <asylbekka@asisnur.kg>
2022-04-30 16:00:53 +0600

    merging submeshes complete, but no a single mesh though

--- Assets/Scripts/MazeBuilder.cs
+++ Assets/Scripts/MazeBuilder.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public class MazeBuilder : MonoBehaviour
 {
@@ -11,150 +12,193 @@ public class MazeBuilder : MonoBehaviour
     [SerializeField]
     private GameObject wallPrefab = null;
     [SerializeField]
-    private GameObject wallPrefabSideWays = null;
-    [SerializeField]
     private GameObject outerWallPrefab = null;
     [SerializeField]
     private GameObject floorPrefab = null;
 
-
     [SerializeField]
     private float spaceOffest = 1f;
     private float spaceOffestHight = 1f;
-
     private Maze maze;
+    private GameObject prefabInstance = null;
+    private List<CombineInstance> wallCI;
+    private List<CombineInstance> outerWallsCI;
+    private List<CombineInstance> floorCI;
     void Start()
     {
+        wallCI = new List<CombineInstance>();
+        outerWallsCI = new List<CombineInstance>();
+        floorCI = new List<CombineInstance>();
         maze = new Maze(mazeRows, mazeColumns, 0, 0);
         maze.BuildMazeData();
         BuildMaze();
-        CombineMeshesInChildren();
+        CombineMeshes();
     }
 
     private void BuildMaze()
     {
-        if (wallPrefab != null && wallPrefabSideWays != null)
+        for (int i = 0; i < mazeRows; i++)
         {
-            for (int i = 0; i < mazeRows; i++)
+            for (int j = 0; j < mazeColumns; j++)
             {
-                for (int j = 0; j < mazeColumns; j++)
+                // check for sideways
+                if ((j < mazeColumns - 1) && (maze.matrix[i, j].blockRight && maze.matrix[i, j + 1].blockLeft))
                 {
-                    // check for sideways
-                    if ((j < mazeColumns - 1) && (maze.matrix[i, j].blockRight && maze.matrix[i, j + 1].blockLeft))
-                    {
-                        GameObject sideWaysWall = new GameObject();
-                        Vector3 rotation = this.transform.rotation.eulerAngles;
-                        rotation = new Vector3(rotation.x, rotation.y + 90, rotation.z);
-                        sideWaysWall = Instantiate(
-                            wallPrefabSideWays,
-                            new Vector3(this.transform.position.x + spaceOffest * j + spaceOffest / 2,
-                                        this.transform.position.y + spaceOffestHight,
-                                        this.transform.position.z + spaceOffest * i),
-                            Quaternion.Euler(rotation),
-                            this.transform);
-                    }
-                    // check for normal
-                    if ((i < mazeRows - 1) && (maze.matrix[i, j].blockDown && maze.matrix[i + 1, j].blockUp))
-                    {
-                        GameObject normalWall = new GameObject();
-                        normalWall = Instantiate(
-                            wallPrefab,
-                            new Vector3(this.transform.position.x + spaceOffest * j,
-                                        this.transform.position.y + spaceOffestHight,
-                                        this.transform.position.z + spaceOffest * i + spaceOffest / 2),
-                            Quaternion.identity,
-                            this.transform);
-                    }
-                    // place floor
-                    if (i > 0 || j > 0)
-                    {
-                        GameObject floor = new GameObject();
-                        floor = Instantiate(
-                            floorPrefab,
-                            new Vector3(this.transform.position.x + spaceOffest * j,
-                                        this.transform.position.y,
-                                        this.transform.position.z + spaceOffest * i),
-                            Quaternion.identity,
-                            this.transform
-                        );
-                    }
-                    // place outer walls, just run once
-                    if (i == 0 && j == 0)
-                    {
-                        GameObject outerWall = new GameObject();
-                        float xPos = 0f;
-                        float zPos = 0f;
-                        float yPos = this.transform.position.z + spaceOffest / 2f;
-                        float xSize = spaceOffest;
-                        float zSize = spaceOffest;
-                        float ySize = 2 * spaceOffest;
-                        // bottom
-                        xPos = this.transform.position.x + spaceOffest * ((mazeColumns - 1) / 2f);
-                        zPos = this.transform.position.z - spaceOffest;
-                        xSize = spaceOffest * (mazeColumns + 1);
-                        zSize = spaceOffest;
-                        outerWall = Instantiate(
-                            outerWallPrefab,
-                            new Vector3(xPos,
-                                        yPos,
-                                        zPos),
-                            Quaternion.identity,
-                            this.transform
-                        );
-                        outerWall.transform.localScale = new Vector3(xSize, ySize, zSize);
-                        outerWall.transform.name = "bottomWall";
-                        // top
-                        xPos = this.transform.position.x + spaceOffest * ((mazeColumns - 1) / 2f);
-                        zPos = this.transform.position.z + spaceOffest * mazeRows;
-                        xSize = spaceOffest * (mazeColumns + 1);
-                        zSize = spaceOffest;
-                        outerWall = Instantiate(
-                            outerWallPrefab,
-                            new Vector3(xPos,
-                                        yPos,
-                                        zPos),
-                            Quaternion.identity,
-                            this.transform
-                        );
-                        outerWall.transform.localScale = new Vector3(xSize, ySize, zSize);
-                        outerWall.transform.name = "topWall";
-                        // left
-                        xPos = this.transform.position.x - spaceOffest;
-                        zPos = this.transform.position.z + spaceOffest * ((mazeRows - 1) / 2f);
-                        xSize = spaceOffest;
-                        zSize = spaceOffest * (mazeRows + 2);
-                        outerWall = Instantiate(
-                            outerWallPrefab,
-                            new Vector3(xPos,
-                                        yPos,
-                                        zPos),
-                            Quaternion.identity,
-                            this.transform
-                        );
-                        outerWall.transform.localScale = new Vector3(xSize, ySize, zSize);
-                        outerWall.transform.name = "leftWall";
-                        // right
-                        xPos = this.transform.position.x + spaceOffest * (mazeColumns);
-                        zPos = this.transform.position.z + spaceOffest * ((mazeRows - 1) / 2f);
-                        xSize = spaceOffest;
-                        zSize = spaceOffest * (mazeRows + 2);
-                        outerWall = Instantiate(
-                            outerWallPrefab,
-                            new Vector3(xPos,
-                                        yPos,
-                                        zPos),
-                            Quaternion.identity,
-                            this.transform
-                        );
-                        outerWall.transform.localScale = new Vector3(xSize, ySize, zSize);
-                        outerWall.transform.name = "rightWall";
-                    }
+                    Vector3 rotation = this.transform.rotation.eulerAngles;
+                    rotation = new Vector3(rotation.x, rotation.y + 90, rotation.z);
+                    prefabInstance = Instantiate(
+                        wallPrefab,
+                        new Vector3(this.transform.position.x + spaceOffest * j + spaceOffest / 2f,
+                                    this.transform.position.y + spaceOffestHight,
+                                    this.transform.position.z + spaceOffest * i),
+                        Quaternion.Euler(rotation),
+                        this.transform);
+                    prefabInstance.transform.localScale = new Vector3(spaceOffest * 1.2f, spaceOffest, spaceOffest * 0.2f);
+                    wallCI.Add(GetCombineInstanceInfo(prefabInstance));
+                }
+                // check for normal
+                if ((i < mazeRows - 1) && (maze.matrix[i, j].blockDown && maze.matrix[i + 1, j].blockUp))
+                {
+                    prefabInstance = Instantiate(
+                        wallPrefab,
+                        new Vector3(this.transform.position.x + spaceOffest * j,
+                                    this.transform.position.y + spaceOffestHight,
+                                    this.transform.position.z + spaceOffest * i + spaceOffest / 2f),
+                        Quaternion.identity,
+                        this.transform);
+                    prefabInstance.transform.localScale = new Vector3(spaceOffest * 1.2f, spaceOffest, spaceOffest * 0.2f);
+                    wallCI.Add(GetCombineInstanceInfo(prefabInstance));
+                }
+                // place floor
+                prefabInstance = Instantiate(
+                    floorPrefab,
+                    new Vector3(this.transform.position.x + spaceOffest * j,
+                                this.transform.position.y,
+                                this.transform.position.z + spaceOffest * i),
+                    Quaternion.identity,
+                    this.transform
+                );
+                floorCI.Add(GetCombineInstanceInfo(prefabInstance));
+
+                // place outer walls, just run once
+                if (i == 0 && j == 0)
+                {
+                    float xPos = 0f;
+                    float zPos = 0f;
+                    float yPos = this.transform.position.z + spaceOffest / 2f;
+                    float xSize = spaceOffest;
+                    float zSize = spaceOffest;
+                    float ySize = 2 * spaceOffest;
+                    // bottom
+                    xPos = this.transform.position.x + spaceOffest * ((mazeColumns - 1) / 2f);
+                    zPos = this.transform.position.z - spaceOffest;
+                    xSize = spaceOffest * (mazeColumns + 1);
+                    zSize = spaceOffest;
+                    prefabInstance = Instantiate(
+                        outerWallPrefab,
+                        new Vector3(xPos,
+                                    yPos,
+                                    zPos),
+                        Quaternion.identity,
+                        this.transform
+                    );
+                    prefabInstance.transform.localScale = new Vector3(xSize, ySize, zSize);
+                    prefabInstance.transform.name = "bottomWall";
+                    outerWallsCI.Add(GetCombineInstanceInfo(prefabInstance));
+                    // top
+                    xPos = this.transform.position.x + spaceOffest * ((mazeColumns - 1) / 2f);
+                    zPos = this.transform.position.z + spaceOffest * mazeRows;
+                    xSize = spaceOffest * (mazeColumns + 1);
+                    zSize = spaceOffest;
+                    prefabInstance = Instantiate(
+                        outerWallPrefab,
+                        new Vector3(xPos,
+                                    yPos,
+                                    zPos),
+                        Quaternion.identity,
+                        this.transform
+                    );
+                    prefabInstance.transform.localScale = new Vector3(xSize, ySize, zSize);
+                    prefabInstance.transform.name = "topWall";
+                    outerWallsCI.Add(GetCombineInstanceInfo(prefabInstance));
+                    // left
+                    xPos = this.transform.position.x - spaceOffest;
+                    zPos = this.transform.position.z + spaceOffest * ((mazeRows - 1) / 2f);
+                    xSize = spaceOffest;
+                    zSize = spaceOffest * (mazeRows + 2);
+                    prefabInstance = Instantiate(
+                        outerWallPrefab,
+                        new Vector3(xPos,
+                                    yPos,
+                                    zPos),
+                        Quaternion.identity,
+                        this.transform
+                    );
+                    prefabInstance.transform.localScale = new Vector3(xSize, ySize, zSize);
+                    prefabInstance.transform.name = "leftWall";
+                    outerWallsCI.Add(GetCombineInstanceInfo(prefabInstance));
+                    // right
+                    xPos = this.transform.position.x + spaceOffest * (mazeColumns);
+                    zPos = this.transform.position.z + spaceOffest * ((mazeRows - 1) / 2f);
+                    xSize = spaceOffest;
+                    zSize = spaceOffest * (mazeRows + 2);
+                    prefabInstance = Instantiate(
+                        outerWallPrefab,
+                        new Vector3(xPos,
+                                    yPos,
+                                    zPos),
+                        Quaternion.identity,
+                        this.transform
+                    );
+                    prefabInstance.transform.localScale = new Vector3(xSize, ySize, zSize);
+                    prefabInstance.transform.name = "rightWall";
+                    outerWallsCI.Add(GetCombineInstanceInfo(prefabInstance));
                 }
             }
         }
     }
 
-    private void CombineMeshesInChildren()
+    private CombineInstance GetCombineInstanceInfo(GameObject obj)
+    {
+        CombineInstance combineInstance = new CombineInstance();
+        combineInstance.mesh = obj.GetComponent<MeshFilter>().sharedMesh;
+        combineInstance.transform = obj.transform.localToWorldMatrix;
+        return combineInstance;
+    }
+
+    private void CombineMeshes()
+    {
+        // deactivate all chidren
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            transform.GetChild(i).gameObject.SetActive(false);
+        }
+        // combine walls
+        prefabInstance = Instantiate(wallPrefab, this.transform.position, Quaternion.identity, this.transform);
+        prefabInstance.GetComponent<MeshFilter>().mesh = new Mesh();
+        prefabInstance.GetComponent<MeshFilter>().mesh.CombineMeshes(wallCI.ToArray(), true);
+        prefabInstance.GetComponent<MeshCollider>().sharedMesh = null;
+        prefabInstance.GetComponent<MeshCollider>().sharedMesh = prefabInstance.GetComponent<MeshFilter>().mesh;
+        prefabInstance.SetActive(true);
+        // combine floor
+        prefabInstance = Instantiate(floorPrefab, this.transform.position, Quaternion.identity, this.transform);
+        prefabInstance.GetComponent<MeshFilter>().mesh = new Mesh();
+        prefabInstance.GetComponent<MeshFilter>().mesh.CombineMeshes(floorCI.ToArray(), true);
+        prefabInstance.GetComponent<MeshCollider>().sharedMesh = null;
+        prefabInstance.GetComponent<MeshCollider>().sharedMesh = prefabInstance.GetComponent<MeshFilter>().mesh;
+        prefabInstance.SetActive(true);
+        // combine combine outerwalls
+        prefabInstance = Instantiate(outerWallPrefab, this.transform.position, Quaternion.identity, this.transform);
+        prefabInstance.GetComponent<MeshFilter>().mesh = new Mesh();
+        prefabInstance.GetComponent<MeshFilter>().mesh.CombineMeshes(outerWallsCI.ToArray(), true);
+        prefabInstance.GetComponent<MeshCollider>().sharedMesh = null;
+        prefabInstance.GetComponent<MeshCollider>().sharedMesh = prefabInstance.GetComponent<MeshFilter>().mesh;
+        prefabInstance.SetActive(true);
+
+        transform.gameObject.SetActive(true);
+    }
+
+    private void CombineMeshByUnity()
     {
         MeshFilter[] meshFilters = GetComponentsInChildren<MeshFilter>();
         CombineInstance[] combine = new CombineInstance[meshFilters.Length];
@@ -168,11 +212,10 @@ public class MazeBuilder : MonoBehaviour
             i++;
         }
         transform.GetComponent<MeshFilter>().mesh = new Mesh();
-        transform.GetComponent<MeshFilter>().mesh.CombineMeshes(combine);
-        transform.GetComponent<MeshCollider>().sharedMesh = null;
-        transform.GetComponent<MeshCollider>().sharedMesh = transform.GetComponent<MeshFilter>().mesh;
+        transform.GetComponent<MeshFilter>().mesh.CombineMeshes(combine, false);
         transform.gameObject.SetActive(true);
     }
+
     void Update()
     {
 
